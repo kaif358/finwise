a6ac0b16fb613579d38ba662a2c7e2c875942e89
kaif358 <kaifchaudhary40@gmail.com>
2025-06-02 14:50:17 +0530

    Getapp.jsx

--- src/components/Getapp.jsx
+++ src/components/Getapp.jsx
@@ -5,7 +5,7 @@ const Getapp = () => {
     <section className='section py-16 md:py-20 lg:py-24 px-4 sm:px-6 md:px-10 lg:px-16 items-center'>
       <div className="w-[280px] h-[612px] md:w-[764px] md:h-[464px] lg:w-[1137px] lg:h-[464px] flex flex-col md:flex-row md:items-center justify-center text-white">
             <div className='order-1 md:order-1 w-full md:w-1/2 flex justify-center items-center'>
-                <img src="/getapp-img.png" loading='lazy' alt="Getapp image" className='w-[200px] md:w-[350px] lg:w-[400px] h-auto' />
+                <img src={`${import.meta.env.BASE_URL}getapp-img.png`} loading='lazy' alt="Getapp image" className='w-[200px] md:w-[350px] lg:w-[400px] h-auto' />
             </div>
             <div className='order-2 md:order-2 w-full md:w-1/2  '>
             <h2 className='text-gray-300 font-normal tracking-widest font-outfit text-[16px] flex flex-col w-[130px]  md:w-[130px]  md:flex-row lg:w-[136px] gap-4 md:gap-8 lg:gap-12  my-5'>GET OUR APP</h2>
